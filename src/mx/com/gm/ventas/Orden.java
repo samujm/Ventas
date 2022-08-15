d0a37edef6ccfdefea5f554903321521b285193f
Samantha González <samujm76@gmail.com>
2022-08-14 23:18:06 -0500

    Creación clase Orden ATRIBUTOS CON ARREGLO CONSTRUCTOR, INICIALIZA LOS ID Y EL ARREGLO

---
+++ src/mx/com/gm/ventas/Orden.java
@@ -0,0 +1,15 @@
+package mx.com.gm.ventas;
+
+public class Orden {
+    private int idOrden;
+    private Producto productos[]; //Plural, solo lo declaro, se inicializa en el constructor
+    private static int contadorOrdenes; //Saber cuantos objetos de tipo producto se han creado y asignar idorden
+    private int contadorProductos; //Saber cuantos productos se agregaron, no es static por que no se relaciona con la clase si no con productos
+    private final static int MAX_PRODUCTOS = 10;
+    
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes; //Preincremento para que ya tenga 1
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+}
